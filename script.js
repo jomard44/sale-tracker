c74ac7dd1e4ff82752e6b12c738480642f7600f2
jomard44 <jomrdali44@gmail.com>
2023-11-14 20:41:46 -0400

    added some commnts

---
+++ script.js
@@ -0,0 +1,20 @@
+/*
+This is a math problem. I want to know a program where this problem can be solved using a program.
+The inputs will be the boxes, bags and  individual flavors, and left after sales and the program will calculate the sold amount.
+Question:
+A store owner buys small, novelty party favours in bags of one dozen and boxes of one dozen bags (for a total of 144 favours in a box).
+The store owner has 7 boxes, 2 bags, and 1 individual party favours at the start of the month. At the end of the month,
+the store owner has 2 boxes, 8 bags, and 6 individual party favours left. The store owner sold how many boxes,
+bags and individual flavors?
+
+*/
+
+function main(){
+    
+//input
+
+//process
+
+//output
+
+}
